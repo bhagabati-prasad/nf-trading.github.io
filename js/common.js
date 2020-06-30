5739653b8aef0a7c2cfce9a336126ac490cd3aa6
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2020-06-30 11:37:54 +0530

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,71 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+$('#owl-carousel').owlCarousel({
+    loop:true,
+    margin:1,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+  $('#owl-testimonial').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+  // end slider
+ 
+ 
+  // end content show hide
+
+  $(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
+
+ // end click to top
+
+ $(document).ready(function() {
+        $(".fancybox").fancybox();
+    });
+ 
+ // click to popup
